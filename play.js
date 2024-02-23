042bc6c8f0e94ea69ea784d2b0005cf265d71e65
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-23 15:14:13 -0500

    Added play.js

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: `localhost`,
+    port: 50541,
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server!");
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // handle incoming data
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
