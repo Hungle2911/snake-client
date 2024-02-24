59eadf24268699e4a79e6fa694374a770a1cadfc
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-24 11:58:12 -0500

    Added client.js and export the connect function then import it in play.js

--- play.js
+++ play.js
@@ -1,22 +1,2 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: `localhost`,
-    port: 50541,
-  });
-  conn.on("connect", () => {
-    console.log("Successfully connected to the game server!");
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  // handle incoming data
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+const connect = require('./client')
+connect
